cbca3839de578271e243df6374c5019bf8ccc86b
PromotezCitizen <gkstkdgus821@gmail.com>
2023-12-24 23:14:35 +0900

    prototype of dictionary

---
+++ Dictionary/dictionary.c
@@ -0,0 +1,166 @@
+#include "dictionary.h"
+#include <stdio.h>
+
+struct dictionary {
+    struct __dict_head head[MAX_HASH];
+};
+
+struct __dict_chain {
+    char* key;
+    int value;
+    struct __dict_chain* next;
+};
+
+struct __dict_head {
+    uint32_t count;
+    struct __dict_chain* next;
+};
+
+struct dictionary dict;
+
+uint32_t appendElement(const char* key, int value) {
+    struct __dict_chain *point;
+    uchar_t hash;
+    int i;
+
+    hash = getHash(key);
+
+    // move to last element
+    point = dict.head[hash].next;
+    for (i = 0; i < dict.head[hash].count; i++) {
+        point = point->next;
+    }
+
+    // create new element
+    point->next = malloc(sizeof(struct __dict_chain));
+    point = point->next;
+
+    // set key and value
+    point->next = NULL;
+    point->key = malloc(sizeof(char) * (strlen(key)+1));
+    strcpy(point->key, key);
+    point->value = value;
+
+    dict.head[hash].count++;
+    return dict.head[hash].count;
+}
+
+uint32_t deleteElement(const char* key) {
+    struct __dict_chain *point, *prev;
+    uchar_t hash;
+
+    hash = getHash(key);
+
+    // unable delete element << no element in dictionary
+    if (dict.head[hash].count == 0) return -1;
+
+    // search key is ${key} element
+    point = dict.head[hash].next;
+    while (strcmp(key, point->key) != 0) {
+        prev = point;
+        point = point->next;
+
+        // if point is pointing NULL, it is error
+        if (point == NULL) return -1;
+    }
+
+    // modify chain
+    if (point == dict.head[hash].next) {
+        dict.head[hash].next = NULL;
+    }
+    else {
+        prev->next = point->next;
+    }
+    free(point);
+
+
+    dict.head[hash].count--;
+    return dict.head[hash].count;
+}
+
+uint32_t modifyValue(const char* key, int value) {
+    struct __dict_chain *point;
+    uchar_t hash;
+
+    hash = getHash(key);
+
+    // unable modify element << no element in dictionary
+    if (dict.head[hash].count == 0) return -1;
+
+    point = dict.head[hash].next;
+    while (strcmp(key, point->key) != 0) {
+        point = point->next;
+
+        // if point is pointing NULL, it is error
+        if (point == NULL) return -1;
+    }
+
+    point->value = value;
+    return 0;
+}
+
+void initDictionary() {
+    int i;
+    for (i = 0; i < MAX_HASH; i++) {
+        dict.head[i].count = 0;
+        dict.head[i].next = NULL;
+    }
+}
+
+void clearDictionary() {
+    int i, j, count;
+    struct __dict_chain *point, *next;
+
+    for (i = 0; i < MAX_HASH; i++) {
+        point = dict.head[i].next;
+        dict.head[i].next = NULL;
+
+        count = dict.head[i].count;
+
+        for (j = 0; j < count; j++) {
+            next = point->next;
+            free(point);
+            point = next;
+        }
+
+        dict.head[i].count = 0;
+    }
+}
+
+uchar_t getHash(const char* key) {
+    uint32_t total;
+    int i, len;
+
+    total = 0;
+    len = strlen(key);
+    for (i = 0; i < len; i++) {
+        total += key[i] % MAX_HASH;
+    }
+    total %= MAX_HASH;
+
+    return total;
+}
+
+void showAllDictionary() {
+    int i;
+
+    for (i = 0; i < MAX_HASH; i++) {
+        showDictionary(i);
+    }
+    return;
+}
+
+void showDictionary(int hash) {
+    struct __dict_chain *point;
+
+    if (hash < 0 || hash >= MAX_HASH) return;
+    printf("datas of hash - %2d\n", hash);
+    point = dict.head[hash].next;
+
+    while (point != NULL) {
+        printf("\t%10s: %10d\n", point->key, point->value);
+        point = point->next;
+    }
+    printf("=====================\n");
+    return;
+}
---
+++ Dictionary/dictionary.h
@@ -0,0 +1,14 @@
+#ifndef __DICT__
+#define __DICT__
+
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_HASH 26
+
+typedef unsigned char uchar_t;
+typedef unsigned int uint32_t;
+
+extern struct dictionary dict;
+
+#endif
